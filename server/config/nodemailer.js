def1df604738ebd3a68cc05dbd72e5896d0b1903
Soumik Jana <soumikjana01@mail.com>
2026-04-11 12:54:26 +0530

    Fix: Force dns resolving to IPv4 to prevent Gmail SMTP connection timeouts

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,4 +1,9 @@
 import nodemailer from 'nodemailer';
+import dns from 'dns';
+
+// Force node to prefer IPv4 over IPv6. This often fixes connection timeouts on cloud hosts like Render
+// when sending mail, as Google's IPv6 SMTP can drop packets from unrecognized cloud IPs.
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
